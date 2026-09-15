56341e7beb66c8dbcd96e797dc178905a0d4943f
qiqqqqq517 <wangyongqi07517@163.com>
2026-09-15 16:53:27 +0800

    docs(website): 官网同步 v3.57.4 + 更新日志追补 v3.57.4 发布说明
    Signed-off-by: qiqqqqq517 <wangyongqi07517@163.com>

--- website/assets/js/site.js
+++ website/assets/js/site.js
@@ -7,8 +7,8 @@
 
 const SITE = {
   name: '上课',
-  version: '3.57.3',
-  versionCode: '227',
+  version: '3.57.4',
+  versionCode: '228',
   /* 主分发渠道：夸克网盘分享。改分享链接只需改这里 */
   quark: 'https://pan.quark.cn/s/02947cbc1d4e',
   quarkCode: '~9a263aTFyF~:/',
