283da9ed017706a28fb4659e1b602867741ecb29
qiqqqqq517 <wangyongqi07517@163.com>
2026-09-24 15:11:03 +0800

    docs(website): 同步 v3.68.0 更新日志 v3.68.0

--- website/assets/js/site.js
+++ website/assets/js/site.js
@@ -7,8 +7,8 @@
 
 const SITE = {
   name: '上课',
-  version: '3.67.0',
-  versionCode: '267',
+  version: '3.68.0',
+  versionCode: '268',
   /* 主分发渠道：夸克网盘分享。改分享链接只需改这里 */
   quark: 'https://pan.quark.cn/s/02947cbc1d4e',
   quarkCode: '~9a263aTFyF~:/',
